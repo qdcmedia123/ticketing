84c9691ae9f3cb42fa493891efd2749b7716bbd2
bharat kumar shah <akashche72@gmail.com>
2021-02-07 15:19:22 +0400

    something is not working

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import {app} from './app';
 
 const start = async() => {
-    console.log('Starting up....');
+    console.log('Starting up....345345');
     if(!process.env.JWT_KEY) {
         throw new Error('JWT key must be defined');
     }
